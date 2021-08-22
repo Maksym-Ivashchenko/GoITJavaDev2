943cfee85fb846ded1a46e4e5b3425a0339c1299
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2021-08-22 14:40:11 +0300

    Update populateDB.sql

--- homeWork3/populateDB.sql
+++ homeWork3/populateDB.sql
@@ -1,90 +1,25 @@
 USE home_work_3_1;
-UPDATE companies SET 
-	company='Company_1',
-	companies_city='Kyiv',
-    companies_email='company_1@mail.com'
-     WHERE companies_id=1;
-     
-UPDATE companies SET 
-	company='Company_2',
-	companies_city='Lviv',
-    companies_email='company_2@mail.com'
-     WHERE companies_id=2;
-     
-UPDATE companies SET 
-	company='Company_3',
-	companies_city='Kharkiv',
-    companies_email='company_3@mail.com'
-     WHERE companies_id=3;
+INSERT INTO companies (company, companies_city, companies_email) VALUES 
+	('Company_1', 'Kyiv', 'company_1@mail.com'),
+    ('Company_2', 'Lviv', 'company_2@mail.com'),
+    ('Company_3', 'Kharkiv', 'company_3@mail.com');
     
-UPDATE customers SET 
-	customer_name='Customer_1',
-	customer_country='Ukraine',
-    customer_email='customer_1@mail.com'
-     WHERE customers_id=1;
+INSERT INTO customers (customer_name, customer_country, customer_email) VALUES 
+	('Customer_1', 'Ukraine', 'customer_1@mail.com'),
+    ('Customer_2', 'Russia', 'customer_2@mail.com'),
+    ('Customer_3', 'France', 'customer_3@mail.com');
 
-UPDATE customers SET 
-	customer_name='Customer_2',
-	customer_country='Russia',
-    customer_email='customer_2@mail.com'
-     WHERE customers_id=2;
+INSERT INTO developers (developers_name, developers_surname, developers_email, developers_gender) VALUES
+	('Ivan', 'Ivanovich', 'ivan@mail.com', 'm'),
+    ('Bogdan', 'Bogdanovich', 'bogdan@mail.com', 'm'),
+    ('Inna', 'Ivich','inna@mail.com', 'w');
 
-UPDATE customers SET 
-	customer_name='Customer_3',
-	customer_country='France',
-    customer_email='customer_3@mail.com'
-     WHERE customers_id=3;
+INSERT INTO projects (projects_name, projects_type, projects_coments) VALUES 
+	('Project_1', 'Java', '1111111'),
+    ('Project_2', 'JavaScript', '222222'),
+    ('Project_3', 'Pyton', '3333333');
 
-UPDATE developers SET developers_name='Ivan',
-	developers_surname='Ivanovich',
-    developers_email='ivan@mail.com',
-    developers_gender='m'
-     WHERE developers_id=1;
-
-UPDATE developers SET developers_name='Bogdan',
-	developers_surname='Bogdanovich',
-    developers_email='bogdan@mail.com',
-    developers_gender='m'
-     WHERE developers_id=2;
-
-UPDATE developers SET developers_name='Inna',
-	developers_surname='Ivich',
-    developers_email='inna@mail.com',
-    developers_gender='w'
-     WHERE developers_id=3;
-
-UPDATE projects SET 
-	projects_name='Project_1',
-	projects_type='Java',
-    projects_coments='1111111'
-     WHERE projects_id=1;
-
-UPDATE projects SET 
-	projects_name='Project_2',
-	projects_type='JavaScript',
-    projects_coments='222222'
-     WHERE projects_id=2;
-
-UPDATE projects SET 
-	projects_name='Project_3',
-	projects_type='Pyton',
-    projects_coments='3333333'
-     WHERE projects_id=3;
-
-UPDATE skills SET 
-	skill_1='+',
-	skill_2='++',
-    skill_3='+++'
-     WHERE skills_id=1;
-
-UPDATE skills SET 
-	skill_1='++',
-	skill_2='+',
-    skill_3='+++'
-     WHERE skills_id=2;
-
-UPDATE skills SET 
-	skill_1='+++',
-	skill_2='++',
-    skill_3='+'
-     WHERE skills_id=3;
+INSERT INTO skills (skill_1, skill_2, skill_3) VALUES 
+	('+', '++', '+++'),
+    ('++', '+', '+++'),
+    ('+++', '++', '+');
