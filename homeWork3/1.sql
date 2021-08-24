224d9a3e5442460972212462fb0755bbc9bf523b
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2021-08-24 21:44:17 +0300

    Update 1.sql

--- homeWork3/1.sql
+++ homeWork3/1.sql
@@ -1,18 +1,53 @@
 USE home_work_3_1;
-ALTER TABLE developers ADD COLUMN developers_speciality VARCHAR(45) AFTER developers_email;
-ALTER TABLE developers ADD COLUMN developers_salary INT AFTER developers_speciality;
+ALTER TABLE developers ADD COLUMN speciality VARCHAR(45) AFTER email;
+ALTER TABLE developers ADD COLUMN salary INT AFTER speciality;
 
 UPDATE developers SET 
-	developers_speciality='Java',
-	developers_salary=1500
-    WHERE developers_id=1;
+	speciality='Java',
+	salary=1500
+    WHERE id=1;
 
 UPDATE developers SET 
-	developers_speciality='Java',
-	developers_salary=2000
-    WHERE developers_id=2;
+	speciality='Java',
+	salary=2000
+    WHERE id=2;
 
 UPDATE developers SET 
-	developers_speciality='Pyton',
-	developers_salary=1700
-    WHERE developers_id=3;
+	speciality='JS',
+	salary=1700
+    WHERE id=3;
+
+UPDATE developers SET 
+	speciality='C++',
+	salary=2100
+    WHERE id=4;
+    
+UPDATE developers SET 
+	speciality='C++',
+	salary=2000
+    WHERE id=5;
+    
+UPDATE developers SET 
+	speciality='JS',
+	salary=1400
+    WHERE id=6;
+    
+UPDATE developers SET 
+	speciality='C#',
+	salary=1800
+    WHERE id=7;
+    
+UPDATE developers SET 
+	speciality='C#',
+	salary=3000
+    WHERE id=8;
+    
+UPDATE developers SET 
+	speciality='C#',
+	salary=2700
+    WHERE id=9;
+    
+UPDATE developers SET 
+	speciality='Java',
+	salary=3700
+    WHERE id=10;
