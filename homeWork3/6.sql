c67137d047e5046d34a74177d228941e5e425440
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2021-08-24 21:52:33 +0300

    Update 6.sql

--- homeWork3/6.sql
+++ homeWork3/6.sql
@@ -1,4 +1,4 @@
 USE home_work_3_1;
     
-SELECT AVG(developers_salary) FROM developers INNER JOIN projects
-	WHERE projects_cost=(SELECT MIN(projects_cost) FROM projects);
+SELECT AVG(salary) FROM developers INNER JOIN projects
+	WHERE cost=(SELECT MIN(cost) FROM projects);
