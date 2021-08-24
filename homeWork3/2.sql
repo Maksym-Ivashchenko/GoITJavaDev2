86c895b7675075c501fec5ba5d58ae4eaf3c064e
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2021-08-24 21:46:25 +0300

    Update 2.sql

--- homeWork3/2.sql
+++ homeWork3/2.sql
@@ -1,4 +1,4 @@
 USE home_work_3_1;
 
-SELECT projects_name FROM projects INNER JOIN developers
-	WHERE developers_salary=(SELECT MAX(developers_salary) FROM developers);
+SELECT project_name FROM projects INNER JOIN developers
+	WHERE salary=(SELECT MAX(salary) FROM developers);
