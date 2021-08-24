c5da82323df5eacd7b70b7aeb1d60052e3ae504e
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2021-08-24 21:50:06 +0300

    Update 4.sql

--- homeWork3/4.sql
+++ homeWork3/4.sql
@@ -1,14 +1,8 @@
 USE home_work_3_1;
-ALTER TABLE projects ADD COLUMN projects_cost INT AFTER projects_type;
+ALTER TABLE projects ADD COLUMN cost INT AFTER project_type;
 
-UPDATE projects SET 
-	projects_cost=27000
-    WHERE projects_id=1;
+UPDATE projects SET cost=27000 WHERE id=1;
     
-UPDATE projects SET 
-	projects_cost=15000
-    WHERE projects_id=2;
+UPDATE projects SET	cost=15000 WHERE id=2;
     
-UPDATE projects SET 
-	projects_cost=30000
-    WHERE projects_id=3;
+UPDATE projects SET cost=30000 WHERE id=3;
