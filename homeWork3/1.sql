8a02041178f3a6ff3e92e91751f29d86270e1a31
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2021-08-26 21:43:32 +0300

    Update 1.sql

--- homeWork3/1.sql
+++ homeWork3/1.sql
@@ -1,53 +1,42 @@
 USE home_work_3_1;
-ALTER TABLE developers ADD COLUMN speciality VARCHAR(45) AFTER email;
-ALTER TABLE developers ADD COLUMN salary INT AFTER speciality;
+ALTER TABLE developer ADD COLUMN salary INT AFTER email;
 
-UPDATE developers SET 
-	speciality='Java',
+UPDATE developer SET 
 	salary=1500
     WHERE id=1;
 
-UPDATE developers SET 
-	speciality='Java',
+UPDATE developer SET 
 	salary=2000
     WHERE id=2;
 
-UPDATE developers SET 
-	speciality='JS',
+UPDATE developer SET 
 	salary=1700
     WHERE id=3;
 
-UPDATE developers SET 
-	speciality='C++',
+UPDATE developer SET 
 	salary=2100
     WHERE id=4;
     
-UPDATE developers SET 
-	speciality='C++',
+UPDATE developer SET 
 	salary=2000
     WHERE id=5;
     
-UPDATE developers SET 
-	speciality='JS',
+UPDATE developer SET 
 	salary=1400
     WHERE id=6;
     
-UPDATE developers SET 
-	speciality='C#',
+UPDATE developer SET 
 	salary=1800
     WHERE id=7;
     
-UPDATE developers SET 
-	speciality='C#',
+UPDATE developer SET 
 	salary=3000
     WHERE id=8;
     
-UPDATE developers SET 
-	speciality='C#',
+UPDATE developer SET 
 	salary=2700
     WHERE id=9;
     
-UPDATE developers SET 
-	speciality='Java',
+UPDATE developer SET 
 	salary=3700
     WHERE id=10;
