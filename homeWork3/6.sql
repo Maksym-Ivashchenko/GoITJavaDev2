71f14ee981742c4204ec2c9a0671df1cf8ee5361
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2021-08-31 22:02:03 +0300

    Update 6.sql

--- homeWork3/6.sql
+++ homeWork3/6.sql
@@ -1,4 +1,10 @@
 USE home_work_3_1;
     
-SELECT AVG(salary) FROM developers INNER JOIN projects
-	WHERE cost=(SELECT MIN(cost) FROM projects);
+SELECT project_name, SUM(cost) AS cost,
+AVG(salary) AS average_salary FROM developer
+INNER JOIN developer_project
+ON developer_project.developer_id = developer.id
+INNER JOIN project
+ON developer_project.project_id = project.id
+GROUP BY project_name ORDER BY cost
+LIMIT 1;
