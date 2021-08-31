8b3989d2303af7d00342a83da59e0b0c577727ba
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2021-08-31 21:49:03 +0300

    Update 3.sql

--- homeWork3/3.sql
+++ homeWork3/3.sql
@@ -1,3 +1,6 @@
 USE home_work_3_1;
 
-SELECT SUM(salary) FROM developers WHERE speciality LIKE 'Java';
+SELECT SUM(salary) AS sum_java_salary FROM developer, developer_skill 
+INNER JOIN skill WHERE branch='Java' 
+AND developer_skill.developer_id=developer.id 
+AND developer_skill.skill_id=skill.id;
